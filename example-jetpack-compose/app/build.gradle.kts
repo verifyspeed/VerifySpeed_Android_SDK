12335966a0f5320f9bc8ced48ab5b5bc33e2424a
RekanDev <rekandev545@gmail.com>
2026-01-01 20:15:10 +0300

    build: Update VerifySpeed Android SDK version to 1.0.34 across all configurations.

--- example-jetpack-compose/app/build.gradle.kts
+++ example-jetpack-compose/app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     //* TIP Add SDK dependency
-    implementation("com.github.verifyspeed:VerifySpeed_Android_SDK:1.0.33")
+    implementation("com.github.verifyspeed:VerifySpeed_Android_SDK:1.0.34")
 
     implementation("com.googlecode.libphonenumber:libphonenumber:8.13.33")
 }
